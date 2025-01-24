c89dc817bd2ac41f56f8f913131fbfed5f55186e
Christopher.Lin <121326666+Aveiro-Lin@users.noreply.github.com>
2025-01-24 13:33:33 +0800

    Update run_damon.sh

--- run_damon.sh
+++ run_damon.sh
@@ -1,3 +1,5 @@
 CUDA_VISIBLE_DEVICES=1 python src/tools/train_deco_contact_damon.py
 
-#上述指令默认不包括使用SMM架构 如果你要使用
+#上述指令默认不包括使用SMM架构 如果你要使用SMM Architecture，请使用如下指令
+
+python src/tools/train_deco_contact_damon .py
